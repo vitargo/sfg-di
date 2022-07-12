8025192a7c793a3acca851ec57ba57ebd2b6ffe9
viktoriia.redko <viktoriia.redko@ericsson.com>
2022-07-12 14:27:48 +0300

    Default Spring Profiles

--- src/main/java/org/vitargo/sfgdi/services/I18nUkrainianGreetingServiceImpl.java
+++ src/main/java/org/vitargo/sfgdi/services/I18nUkrainianGreetingServiceImpl.java
@@ -3,7 +3,7 @@ package org.vitargo.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("UA")
+@Profile({"UA", "default"})
 @Service("i18nService")
 public class I18nUkrainianGreetingServiceImpl implements GreetingService{
 
